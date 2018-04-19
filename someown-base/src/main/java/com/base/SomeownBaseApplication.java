39bd9f9539f4f15cdfdab21efd9a1f889503531e
he <he@He>
2018-04-19 23:19:41 +0200

    springboot

---
+++ someown-base/src/main/java/com/base/SomeownBaseApplication.java
@@ -0,0 +1,12 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeownBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeownBaseApplication.class, args);
+	}
+}
